aa94ad72f16f85f4af59864c11801d1e9c91a7f8
devwillians <vwillians22@gmail.com>
2024-03-20 23:25:08 -0500

    ajustando configuraciones en el main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom'; // Importa ReactDOM desde react-dom
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
