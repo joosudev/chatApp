a47aadb26019bce61cc063487622a2cccf33efcb
devwillians <vwillians22@gmail.com>
2024-03-21 02:40:07 -0500

    configuraciones

--- server/index.js
+++ server/index.js
@@ -4,19 +4,12 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const path = require("path");
 
-// Create an Express app
 const app = express();
-// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 
-// Define paths for static files
-const publicDirectoryPath = path.join(__dirname, '../client');
-
-
-// Serve static files
+const publicDirectoryPath = path.join(__dirname, '../client/src');
 app.use(express.static(publicDirectoryPath));
 
-// Create an HTTP server using the Express app
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -26,24 +19,17 @@ const io = new Server(server, {
   },
 });
 
-// Event handler when a new socket connection is established
 io.on("connection", (socket) => {
   console.log(`a user connected ${socket.id}`);
-
-  // Event handler for receiving messages from clients
   socket.on("message", (message) => {
     console.log("message:", message);
-    // Broadcast the received message to all connected clients
     io.emit("message", message);
   });
-
-  // Event handler when a client disconnects
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
 
-// Define the port number for the server to listen on
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
